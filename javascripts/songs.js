ee7dc48db6fa95517fbb3dc5b896cc7a9b38b9a4
Edward R <edwardrz@users.noreply.github.com>
2017-08-20 16:46:44 -0500

    Replaced '>' with '-'.

--- javascripts/songs.js
+++ javascripts/songs.js
@@ -1,3 +1,8 @@
+//QUESTIONS
+// [ ] line 23:  why does for loop need to count down instead of up? Count down vs Up?
+	//why does adding "=" make line 23 undefined?
+
+
 var songs = [];
 
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
@@ -6,6 +11,38 @@ songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album Th
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
-songs.unshift("Array's First Song");//Adds song to beginning of array.
-songs.push("Array's Last Song");//Adds song to end of array
+songs.unshift("Array's First Song - by The Arrays");//Adds song to beginning of array.
+songs.push("Array's Last Song - by the Arrays");//Adds song to end of array
 
+//Remove characters * @ ( !
+//use indexOf, splice
+
+// Find and remove item from an array
+for(var i = songs.length-1; i >= 0; i--){  // STEP 1
+    if(songs[i] == '@'){              // STEP 2
+		console.log("splice @   ", songs[i]);
+        songs.splice(i,1);                 // STEP 3
+    }
+}
+
+
+
+//@@@@@@@@@@@@@@
+// for (i=0; i<songs.length; i+=1) {
+// 	var x = songs.indexOf("*");
+// 			if(x != -1) {
+// 				songs.splice(x, 1);
+// 		}
+// }
+
+
+//Find ">" and replace with "-" using replace method
+// for (i = songs.length-1; i >= 0; i-=1){
+// for (i=0; i<songs.length; i+=1) {
+// 			songs[i] = songs[i].replace(">", "-");
+// 			console.log("Replaced (>):  ", songs[i]);
+// }
+
+
+
+	
